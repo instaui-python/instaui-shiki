5fcc3e0133a319602c23c83cef092ea63549b431
CrystalWindSnake <568166495@qq.com>
2025-09-21 23:14:18 +0800

    override add param others

--- src/instaui_shiki/cdn/_cdn.py
+++ src/instaui_shiki/cdn/_cdn.py
@@ -6,12 +6,19 @@ from instaui_shiki import consts
 def override(
     *,
     shiki_code_logic_js: Optional[str] = None,
+    others: Optional[dict[str, str]] = None,
 ) -> CdnResourceOption:
-    if not shiki_code_logic_js:
+    if not shiki_code_logic_js and not others:
         return default_override()
 
+    import_maps = {}
+    if others:
+        import_maps.update(others)
+    if shiki_code_logic_js:
+        import_maps[consts.SHIKI_CODE_LOGIC_IMPORT_NAME] = shiki_code_logic_js
+
     return CdnResourceOption(
-        import_maps={consts.SHIKI_CODE_LOGIC_IMPORT_NAME: shiki_code_logic_js},
+        import_maps=import_maps,
     )
 
 
