5efb7eecb06b16c311ea2ab09a2d6f2056710fb6
CrystalWindSnake <36034954+CrystalWindSnake@users.noreply.github.com>
2025-10-18 19:05:18 +0800

    feat: localize "Copied" message based on app language (#3)

--- npm-shiki/types/instaui.d.ts
+++ npm-shiki/types/instaui.d.ts
@@ -1,5 +1,9 @@
+import type { Ref } from "vue";
+
 declare module "instaui" {
   export function useBindingGetter(): {
     getValue: (key: any) => any;
   };
+
+  export function useLanguage(): Ref<string>;
 }
